934ddac3be4017392a5cdf63ee99c3a72c1cfa3a
katevaselkova <katevaselkova@me.com>
2021-05-24 18:17:11 +0300

    New changes

--- HomeWorkAPITest/src/test/java/org/example/api/ApiTest.java
+++ HomeWorkAPITest/src/test/java/org/example/api/ApiTest.java
@@ -17,100 +17,76 @@ import java.util.UUID;
 import static io.restassured.RestAssured.given;
 
 public class ApiTest {
+    private Pet pet;
+    private int id;
+    private String name;
+
     @BeforeClass
     public void prepare() throws IOException {
-
-        // Читаем конфигурационный файл в System.properties -- простейшее HashMap хранилище
-        System.getProperties().load(ClassLoader.getSystemResourceAsStream("my.properties"));
-
-        // Здесь мы задаём глобальные преднастройки для каждого запроса. Аналогично можно задавать их
-        // перед каждым запросом отдельно, либо создать поле RequestSpecification и задавать весь пакет настроек
-        // в конкретных запросах. Подробнее тут: https://habr.com/ru/post/421005/
-        RestAssured.requestSpecification = new RequestSpecBuilder()
-                .setBaseUri("https://petstore.swagger.io/v2/") // задаём базовый адрес каждого ресурса
-                .addHeader("api_key", System.getProperty("api.key")) // задаём заголовок с токеном для авторизации
-                // обязательно учитывайте, что любые приватные данные необходимо хранить в отдельных файлах, которые НЕ публикуютя
-                // в открытых репозиториях (в закрытых тоже лучше не публиковать)
-                .setAccept(ContentType.JSON) // задаём заголовок accept
-                .setContentType(ContentType.JSON) // задаём заголовок content-type
-                .log(LogDetail.ALL) // дополнительная инструкция полного логгирования для RestAssured
-                .build(); // после этой команды происходит формирование стандартной "шапки" запроса.
-        // Подробнее о билдерах можно почитать https://refactoring.guru/ru/design-patterns/builder
-        // но интереснее в книжке Effective Java
-
-        //Здесь задаётся фильтр, позволяющий выводить содержание ответа,
-        // также к нему можно задать условия в параметрах конструктора, которм должен удовлетворять ответ (например код ответа)
-        RestAssured.filters(new ResponseLoggingFilter());
-    }
-
-    /**
-     * Простейшая проверка: создаём объект, сохраняем на сервере и проверяем, что при запросе возвращается
-     * "тот же" объект
-     */
-    @Test
-    public void checkObjectSave() {
-        Pet pet = new Pet(); // создаём экземпляр POJO объекта Pet
-        int id = new Random().nextInt(500000); // просто нужно создать произвольный айди
-        String name = "Pet_" + UUID.randomUUID().toString(); // UUID гарантирует уникальность строки
+        pet = new Pet();
+        id = new Random().nextInt(500000);
+        name = "Pet_" + UUID.randomUUID().toString();
         pet.setId(id);
         pet.setName(name);
 
-        given()  // часть стандартного синтаксиса BDD. Означает предварительные данные. Иначе говоря ДАНО:
-                .body(pet) // указываем что  помещаем в тело запроса. Поскольку у нас подключен Gson, он преобразуется в JSON
-            .when()   // КОГДА:
-                .post("/pet") // выполняем запрос методом POST к ресурсу /pet, при этом используется ранее
-                // созданная "шапка". Именно в этом методе создаётся "текстовый файл" запроса, он отправляется
-                // посредством HTTP к серверу. Затем в этом же методе получается ответ. После этого метода мы
-                // работаем с ОТВЕТОМ
-            .then() // ТОГДА: (указывает, что после этой части будут выполняться проверки-утверждения)
-                .statusCode(200); // например проверка кода ответа.он просто выдёргивается из текста ответа
+        System.getProperties().load(ClassLoader.getSystemResourceAsStream("my.properties"));
 
-        /*
-         * Подобный стиль написания кода называется fluent -- текучим. Мы последовательно вызываем методы, при этом
-         * объект у которого вызываются методы может меняться по ходу вызовов. метод возвращает какой-то объект,
-         * следующий в цепочке метод вызывается у него, он возвращает ещё какой-то объект (или тот же), и следующий
-         * метод вызывается уже у него.
-         * Подобный подход характерен для билдеров (например похоже работают Stream, Fluent Wait и многие другие).
-         * Он позволяет сократить количество кода, однако может вызывать некоторое непонимание у новичков. Idea
-         * помогает понять какой объект возвращается после метода... а остальное дело привычки
-         * */
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("https://petstore.swagger.io/v2/")
+                .addHeader("api_key", System.getProperty("api.key"))
+                .setContentType(ContentType.JSON)
+                .log(LogDetail.ALL)
+                .build();
+
+        RestAssured.filters(new ResponseLoggingFilter());
+    }
+
+    @Test
+    public void checkObjectSave() {
+
+        given()
+                .body(pet)
+                .when()
+                .post("/pet")
+                .then()
+                .statusCode(200);
 
         Pet actual =
                 given()
-                        .pathParam("petId", id) // заранее задаём переменную petId
-                    .when()
-                        .get("/pet/{petId}") // которая подставится в путь ресурса перед выполнением запроса.
-                        // после этого метода мы так же будем иметь уже ОТВЕТ от сервера.
-                    .then()
+                        .pathParam("petId", id)
+                        .when()
+                        .get("/pet/{petId}")
+                        .then()
                         .statusCode(200)
-                        .extract().body() // у полученного ответа мы можем взять тело
-                        .as(Pet.class); // и распарсить его как объект Pet. Всё это получается автоматически, так как
-                        // у нас подключена библиотека для работы с JSON и мы дополнительно указали в общей "шапке"
-                        // что хотим получать и отправлять объекты в формате JSON
-        // Здесь сравниваем только имя, поскольку многие поля у наших объектов не совпадают: поскольку
-        // мы не задали список тэгов животного, в объекте pet он будет null, тогда как в объекте actual Gson присвоит
-        // ему пустой список. Это происходит потому что в ответ всегда приходит полный JSON модели
-        // (как описано в Swagger.io), даже если мы отправляли не полную модель.
-        // TODO можно переопределить методы equals у объектов Pet и других, чтобы происходило корректное сравнение
-        // не заданных полей с пустыми
+                        .extract().body()
+                        .as(Pet.class);
+
         Assert.assertEquals(actual.getName(), pet.getName());
 
     }
 
     @Test
-    public void tetDelete() throws IOException {
+    public void testDelete() throws IOException {
+
+        given()
+                .body(pet)
+                .when()
+                .post("/pet")
+                .then()
+                .statusCode(200);
+
         System.getProperties().load(ClassLoader.getSystemResourceAsStream("my.properties"));
         given()
-                .pathParam("petId", System.getProperty("petId"))
-            .when()
+                .pathParam("petId", id)
+                .when()
                 .delete("/pet/{petId}")
-            .then()
+                .then()
                 .statusCode(200);
         given()
-                .pathParam("petId", System.getProperty("petId"))
-             .when()
+                .pathParam("petId", id)
+                .when()
                 .get("/pet/{petId}")
-             .then()
+                .then()
                 .statusCode(404);
     }
 }
--- HomeWorkAPITest/src/test/java/org/example/api/HomeTaskApiTest.java
+++ HomeWorkAPITest/src/test/java/org/example/api/HomeTaskApiTest.java
@@ -10,17 +10,35 @@ import org.example.model.Store;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import sun.security.mscapi.CPublicKey;
+
 import static io.restassured.RestAssured.given;
+
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
 public class HomeTaskApiTest {
+    private int id;
+    private Store store;
+    private int petId;
+    private int quantity;
 
     @BeforeClass
     public void prepare() throws IOException {
+        id = new Random().nextInt(500000);
+        store = new Store();
+        petId = new Random().nextInt(20000);
+        quantity = new Random().nextInt(50);
+        store.setId(id);
+        store.setPetId(petId);
+        store.setQuantity(quantity);
+        store.setShipDate("2021-05-20T11:28:29.632Z");
+        store.setStatus("placed");
+        store.setComplete(true);
         System.getProperties().load(ClassLoader.getSystemResourceAsStream("my.properties"));
-        RestAssured.requestSpecification = (RequestSpecification) new RequestSpecBuilder()
+        RestAssured.requestSpecification = new RequestSpecBuilder()
                 .setBaseUri("https://petstore.swagger.io/v2/")
                 .addHeader("api_key", System.getProperty("api.key"))
                 .setAccept(ContentType.JSON)
@@ -31,18 +49,18 @@ public class HomeTaskApiTest {
     }
 
     @Test
-    public void checkObjectSave() {
-        Store store = new Store();
-        int id = new Random().nextInt(500000);
-        int petId = new Random().nextInt(20000);
-        int quantity = new Random().nextInt(50);
-        store.setId(id);
-        store.setPetId(petId);
-        store.setQuantity(quantity);
-        store.setShipDate("2021-05-20T11:28:29.632Z");
-        store.setStatus("placed");
-        store.setComplete(true);
+    public void checkObjectPost() {
 
+        given().
+                body(store)
+                .when()
+                .post("store/order")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void checkObjectGet() {
         given().
                 body(store)
                 .when()
@@ -60,7 +78,16 @@ public class HomeTaskApiTest {
                         .extract().body()
                         .as(Store.class);
         Assert.assertEquals(actual.getPetId(), store.getPetId());
+    }
 
+    @Test
+    public void checkObjectDelete() {
+        given().
+                body(store)
+                .when()
+                .post("store/order")
+                .then()
+                .statusCode(200);
 
         given()
                 .pathParam("petId", id)
@@ -68,6 +95,17 @@ public class HomeTaskApiTest {
                 .delete("/store/order/{petId}")
                 .then()
                 .statusCode(200);
+    }
+
+    @Test
+    public void checkObjectGetError() {
+        given()
+                .pathParam("petId", id)
+                .when()
+                .delete("/store/order/{petId}")
+                .then()
+                .statusCode(200);
+
         given()
                 .pathParam("petId", id)
                 .when()
@@ -78,15 +116,23 @@ public class HomeTaskApiTest {
 
     @Test
     public void inventory() {
+
         Map map =
                 given()
-                .when()
-                .get("/store/inventory")
-                .then()
+                        .when()
+                        .get("/store/inventory")
+                        .then()
                         .statusCode(200)
                         .extract()
                         .body()
                         .as(Map.class);
-        Assert.assertTrue(map.containsKey("sold"), "Inventory не содержит статус sold" );
+        Assert.assertTrue(map.containsKey("sold"), "Inventory не содержит статус sold");
+
+        Assert.assertTrue(map.get("sold") instanceof Double);
+        Assert.assertTrue(map.get("pending") instanceof Double);
+        Assert.assertTrue(map.get("free") instanceof Double);
+        Assert.assertTrue(map.get("available") instanceof Double);
+        Assert.assertTrue(map.get("412") instanceof Double);
+        Assert.assertTrue(map.get("44422") instanceof Double);
     }
 }
